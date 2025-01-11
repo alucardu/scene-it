5ae9344ca2fccfb4c3ba671a3662b490bdd472f8
Peter Boomsma <peter@cleanpixel.nl>
2025-01-11 20:42:47 +0100

    Show current guess of current player

--- src/app/guess/guesses/guesses.component.ts
+++ src/app/guess/guesses/guesses.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { MatList, MatListItem } from '@angular/material/list';
   import { SessionService } from '../../session/session.service';
+import { AuthService } from '../../auth/auth.service';
+import { GuessService } from '../guess.service';
 
 @Component({
   selector: 'app-guesses',
@@ -10,6 +12,10 @@ import { MatList, MatListItem } from '@angular/material/list';
   imports: [MatListItem, MatCard, MatCardContent, MatCardHeader, MatList, MatCardTitle]
 })
 export class GuessesComponent {
+  private authService = inject(AuthService);
   private sessionService = inject(SessionService);
+  private guessService = inject(GuessService)
+
   sessionResource = this.sessionService.sessionResource;
+  currentGuessResource = this.guessService.currentGuessResource;
 }
